69f20bb5c15de956d7470de1b5a119276cc31bde
trabalhadorcansado <74340426+trabalhadorcansado@users.noreply.github.com>
2021-01-23 06:06:03 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,100 @@
+$(function() {
+  $('#colorselector').change(function(){
+    $('.colors').hide();
+    $('.prelim').hide();
+    $('#' + $(this).val()).show();
+  });
+});
+
+$(function() {
+  $('#preliminarselector').change(function(){
+    $('.colors').hide();
+    $('.prelim').hide();
+    $('#' + $(this).val()).show();
+  });
+});
+
+$(function() {
+  $('#principalselector').change(function(){
+    $('.colors').hide();
+    $('.prelim').hide();
+    $('#' + $(this).val()).show();
+  });
+});
+
+$(function() {
+  $('#winnerselector').change(function(){
+    $('.prelim').hide();
+    $('#' + $(this).val()).show();
+  });
+});
+
+$(function() {
+  $('#winnerselector2').change(function(){
+    $('.prelim').hide();
+    $('#' + $(this).val()).show();
+  });
+});
+
+$(function() {
+  $('#winnerselector3').change(function(){
+    $('.prelim').hide();
+    $('#' + $(this).val()).show();
+  });
+});
+
+$(function() {
+  $('#winnerselector4').change(function(){
+    $('.prelim').hide();
+    $('#' + $(this).val()).show();
+  });
+});
+
+$(function() {
+  $('#winnerselector5').change(function(){
+    $('.prelim').hide();
+    $('#' + $(this).val()).show();
+  });
+});
+
+$(function() {
+  $('#winnerselector6').change(function(){
+    $('.prelim').hide();
+    $('#' + $(this).val()).show();
+  });
+});
+
+$(function() {
+  $('#winnerselector7').change(function(){
+    $('.prelim').hide();
+    $('#' + $(this).val()).show();
+  });
+});
+
+$(function() {
+  $('#winnerselector8').change(function(){
+    $('.prelim').hide();
+    $('#' + $(this).val()).show();
+  });
+});
+
+$(function() {
+  $('#winnerselector9').change(function(){
+    $('.prelim').hide();
+    $('#' + $(this).val()).show();
+  });
+});
+
+$(function() {
+  $('#winnerselector10').change(function(){
+    $('.prelim').hide();
+    $('#' + $(this).val()).show();
+  });
+});
+
+$(function() {
+  $('#winnerselector11').change(function(){
+    $('.prelim').hide();
+    $('#' + $(this).val()).show();
+  });
+});
